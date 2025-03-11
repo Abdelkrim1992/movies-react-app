e79f157e62f852379093cffd62bb23872f82e8cb
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-03-11 12:28:10 +0000

    update search

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+const Search = ({search, setSearch}) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="./search.svg" alt="search"/>
+        <input type="text" placeholder='Search for a movie' value={search} onChange={(e)=> setSearch(e.target.value)}/> 
+      </div>
+    </div>
+  )
+}
+
+export default Search
