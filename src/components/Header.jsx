5ec8d94dce109e28f34ee241a3f6ba3ee93be1c5
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-03-11 12:27:33 +0000

    update header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <section>
+        <header>
+          <img src="./hero.png" alt="hero" />
+          <h1>
+            Find The{" "}
+            <span className="text-gradient">
+              Best <br /> Movies{" "}
+            </span>
+            Right Now
+          </h1>
+        </header>
+    </section>
+  )
+}
+
+export default Header
